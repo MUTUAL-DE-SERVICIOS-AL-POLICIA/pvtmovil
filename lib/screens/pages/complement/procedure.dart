7ed089f411055978d7364c76d4ba1ad4c783f658
moisicmo <46501241+moisicmo@users.noreply.github.com>
2022-10-15 15:33:43 -0400

    lintoptions

--- lib/screens/pages/complement/procedure.dart
+++ lib/screens/pages/complement/procedure.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
